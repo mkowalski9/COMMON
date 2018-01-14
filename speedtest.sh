a04204eca1b14fbf557cf8eeeaa859817b1f9bb3
Silent Treatment <mikekayt@gmail.com>
2018-01-13 21:09:52 -0500

    Initial speedtest script

---
+++ speedtest.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+### Test Internet Speed
+
+echo "Testing Internet Speed via [curl : speedtest.py"]
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+
